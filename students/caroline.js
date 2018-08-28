ddd08e65b4ae73cdab045189e87b3af9003520c7
Caroline Romero <cgm.romero@gmail.com>
2018-08-28 17:00:30 +0200

    added search field

--- students/caroline.js
+++ students/caroline.js
@@ -1,14 +1,45 @@
 'use strict';
 
 function main() {
-  var nodes = document.querySelectorAll('section h3');
-  
-  for (var ix = 0; ix < nodes.length; ix++) {
-    nodes[ix].addEventListener('click', function (event) {
-      var h3 = event.currentTarget;
-      var section = h3.parentNode;
-      section.classList.toggle('expanded');
-    });
+
+  function findStudents(terms){
+    var results = [];
+    if (terms){
+      results = students.filter(function(student){
+      return student.name.toLowerCase().indexOf(terms) >= 0;
+      });
+    };
+    return results;
   };
-}
+
+  function displayResults(results){
+
+    var searchResults = document.querySelector('.quick-nav .search-results');
+    searchResults.innerHTML = "";
+
+    var ul = document.createElement('ul');
+
+    results.forEach(function(student){
+      var link = document.createElement('a');
+      link.innerText = student.name;
+      link.setAttribute('href', "../" + student.url);   
+      
+      var li = document.createElement('li');
+      li.appendChild(link);
+      ul.appendChild(li);
+    })
+    searchResults.appendChild(ul);
+  };
+
+  function handleChange(){   
+    var searchTerms = input.value.toLowerCase();
+    var results = findStudents(searchTerms);
+    displayResults(results);
+  };
+
+  var input = document.querySelector(".quick-nav input");
+  input.addEventListener('keyup', handleChange);
+};
+
 window.addEventListener('load', main);
+
